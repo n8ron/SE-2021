0603ea36d3f8b0f693ab3d582aab8d714f108209
n8ron <nik_abramov22@mail.ru>
2021-11-02 22:01:49 +0300

    add test cov

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.31"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    jacoco
 }
 
 group = "ru.hse"
@@ -13,11 +14,27 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit"))
+    implementation("org.jacoco:org.jacoco.core:0.8.7")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.5.31")
+}
+
+jacoco {
+    toolVersion = "0.8.7"
 }
 
 tasks.test {
     useJUnit()
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+    }
+    dependsOn(tasks.test)
 }
 
 ktlint {
