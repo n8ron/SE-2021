8192cc937b67cbfbd6a36867b17ec51b6db5cfe6
Roman Venediktov <e2e4b6b7@gmail.com>
2021-11-03 14:52:01 +0300

    Add tests for Availability constructor throws exception

--- src/test/kotlin/ru/hse/plameet/core/constraints/AvailabilityConstraintTest.kt
+++ src/test/kotlin/ru/hse/plameet/core/constraints/AvailabilityConstraintTest.kt
@@ -2,6 +2,7 @@ package ru.hse.plameet.core.constraints
 
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -72,6 +73,42 @@ object AvailabilityConstraintTest {
                     schedule(
                         TimedEvent(Event(0, Duration(1), listOf(User(1), User(2))), TimeStamp(0)),
                         TimedEvent(Event(0, Duration(1), listOf(User(1))), TimeStamp(4))
+                    )
+                )
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideConstructorThrows")
+    fun testConstructorThrows(av: Map<User, List<TimeRange>>) {
+        assertThrows<IllegalArgumentException> { AvailabilityConstraint(av) }
+    }
+
+    @JvmStatic
+    private fun provideConstructorThrows(): Stream<Arguments> {
+        return Stream.of(
+            Arguments.of(
+                mapOf(
+                    User(1) to listOf(
+                        TimeStamp(0).until(TimeStamp(6)),
+                        TimeStamp(5).until(TimeStamp(8))
+                    ),
+                    User(2) to listOf(
+                        TimeStamp(0).until(TimeStamp(1)),
+                        TimeStamp(3).until(TimeStamp(8))
+                    )
+                )
+            ),
+            Arguments.of(
+                mapOf(
+                    User(1) to listOf(
+                        TimeStamp(0).until(TimeStamp(4)),
+                        TimeStamp(5).until(TimeStamp(8))
+                    ),
+                    User(2) to listOf(
+                        TimeStamp(0).until(TimeStamp(19)),
+                        TimeStamp(3).until(TimeStamp(8))
                     )
                 )
             )
