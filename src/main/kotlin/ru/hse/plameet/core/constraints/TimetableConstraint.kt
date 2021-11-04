4669b766f6608840c47ab19896a82771fa02f062
Roman Venediktov <e2e4b6b7@gmail.com>
2021-11-04 17:04:33 +0300

    Remove not environment

--- src/main/kotlin/ru/hse/plameet/core/constraints/TimetableConstraint.kt
+++
@@ -1,8 +0,0 @@
-package ru.hse.plameet.core.constraints
-
-import ru.hse.plameet.core.Duration
-import ru.hse.plameet.core.TimeStamp
-import ru.hse.plameet.core.during
-
-open class TimetableConstraint(slots: List<TimeStamp>, duration: Duration) :
-    SlotsConstraint(slots.map { it.during(duration) })
--- src/test/kotlin/ru/hse/plameet/core/TestUtils.kt
+++
@@ -1,5 +0,0 @@
-package ru.hse.plameet.core
-
-fun schedule(vararg timedEvent: TimedEvent): Schedule {
-    return Schedule(timedEvent.toList())
-}
